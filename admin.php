bd9163cd133d0e7c95cdcbfea3488da32fa536ba
Danil <waisee7@gmail.com>
2019-08-16 14:26:17 +0300

    - add using twig for admin panel

--- admin.php
+++ admin.php
@@ -2,17 +2,28 @@
 
 require_once 'init.php';
 
+
+
 $users = getAllUsers($pdo);
 $orders = getAllOrders($pdo);
 
-echo 'Пользователи:<br>';
-foreach ($users as $user){
-    echo 'id:' . $user['id'] . ' | email: ' . $user['email'] . ' | телефон: ' . $user['phone'] . '<br><br>';
-}
+//echo 'Пользователи:<br>';
+//foreach ($users as $user){
+//    echo 'id:' . $user['id'] . ' | email: ' . $user['email'] . ' | телефон: ' . $user['phone'] . '<br><br>';
+//}
+//
+//echo 'Заказы:<br>';
+//echo 'id | user_id | street | home | appt | floor | comment | payment | callback<br><hr>';
+//foreach ($orders as $order){
+//    echo implode(' | ', $order);
+//    echo '<br>';
+//}
 
-echo 'Заказы:<br>';
-echo 'id | user_id | street | home | appt | floor | comment | payment | callback<br><hr>';
-foreach ($orders as $order){
-    echo implode(' | ', $order);
-    echo '<br>';
-}
+$path = __DIR__ . '/templates/';
+$loader = new \Twig\Loader\FilesystemLoader($path);
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render('admin.twig', [
+    'users' => $users,
+    'orders' => $orders,
+    ]);
